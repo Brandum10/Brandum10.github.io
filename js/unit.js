1f9cb4a384549c2fd09cc6694cac664d61317134
Brandum10 <50757035+Brandum10@users.noreply.github.com>
2021-03-22 19:08:58 -0600

    Update unit.js

--- js/unit.js
+++ js/unit.js
@@ -1,4 +1,9 @@
 firebase.initializeApp({
-  
-  
+  apiKey: "AIzaSyDnhDqS3SGGdWGQBt3_06bl9RrVeRU4DQE",
+    authDomain: "autentifica43omb-a3023.firebaseapp.com",
+    projectId: "autentifica43omb-a3023",
+    storageBucket: "autentifica43omb-a3023.appspot.com",
+    messagingSenderId: "949658339877",
+    appId: "1:949658339877:web:7d581d98ea2eb707994dcc",
+    measurementId: "G-HVL7KDZEKV"
    });
